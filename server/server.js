3bd6544aea4ddb2bfdfa430fe2f2d1e0a2e72f00
Achyut <aoogleak@gmail.com>
2022-10-05 13:27:05 +0545

    1.9 css and html added

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket)=>{
+    console.log("New User Connected!");
+        
+    socket.on('disconnect', () => {
+            console.log("A user just disconnected!");
+    });
+});
+
 
 server.listen( process.env.PORT || 3000, ()=>{
     console.log("Server running on Port 3000");
