c26a1b997b50d835f5faae7502b263f614d47443
Achyut <aoogleak@gmail.com>
2022-10-05 11:56:49 +0545

    1.2 index file and server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "/../public");
 const app = express();
-
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000 || process.env.PORT, ()=>{
+
+server.listen(3000 || process.env.PORT, ()=>{
     console.log("Server running on Port 3000");
-})
+});
